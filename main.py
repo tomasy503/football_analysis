a8c931975f23d603205dfe2f2b487808b0ba9b03
Tomas Yanez <tomas.yanez86@gmail.com>
2024-09-10 16:22:01 +0200

    added object detection and tracking for players and ball

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from trackers import Tracker
 from utils import read_video, save_video
 
 
@@ -5,8 +6,20 @@ def main():
     # Read video
     video_frames = read_video("data/raw/bmg_test.mp4")
 
+    # Initialize tracker
+    tracker = Tracker("model/best.pt")
+
+    # Detect objects in video frames
+    detections = tracker.object_detection(
+        video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl"
+    )
+
+    # draw output
+    # draw object annotations
+    output_video_frames = tracker.draw_annoatations(video_frames, detections)
+
     # Save video
-    save_video(video_frames, "data/processed/output_video.mp4")
+    save_video(output_video_frames, "data/processed/output_video.mp4")
 
 
 if __name__ == "__main__":
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,2 @@
+from .bbox_utils import get_bbox_width, get_center_of_bbox
 from .video_utils import read_video, save_video
